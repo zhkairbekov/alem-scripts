2aad5fae6a06b200c61f681cdacc9c6bfb3729d5
zhkairbek <kazah.zanat@gmail.com>
2025-07-24 17:18:07 +0500

    gitconfig

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Проверка, передан ли аргумент
+if [ -z "$1" ]; then
+  echo "Ошибка: не указан комментарий для коммита."
+  echo "Использование: ./gitconfig.sh \"commit_message\""
+  exit 1
+fi
+
+# Сохранение аргумента как комментария
+commit_msg="$1"
+
 # Добавить все файлы
 git add .
-
-# Спросить у пользователя комментарий коммита
-read -p "Введите комментарий для коммита: " commit_msg
 
 # Сделать коммит
 git commit -m "$commit_msg"
@@ -14,6 +21,3 @@ current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Сделать push
 git push origin "$current_branch"
-
-
-# ./gitconfig.sh - для запуска
